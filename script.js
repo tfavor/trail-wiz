02711c5ba2c14b2fceb6414518ac73a6d0542f78
Tristan Favor <tristanfavor29@gmail.com>
2019-07-04 17:59:29 -0500

    compleeted wireframe

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ $(function changeHike() {
     $(".hike").on('click', function(event) {
         event.preventDefault();
         $("header").addClass('hidden');
-        $(".main-content").removeClass('hidden');
+        $(".nav-bar-container").removeClass('hidden');
+        $(".main-search-container").removeClass('hidden');
     })
 })
 
@@ -11,6 +12,45 @@ $(function changebike() {
     $(".bike").on('click', function(event) {
         event.preventDefault();
         $("header").addClass('hidden');
-        $(".main-content").removeClass('hidden');
+        $(".nav-bar-container").removeClass('hidden');
+        $(".main-search-container").removeClass('hidden');
     })
 })
+
+$(function navigateHike() {
+    $(".hiking-option").on('click', function(event) {
+        event.preventDefault();
+        $(".main-search-container").removeClass('hidden');
+        $(".results-container").addClass('hidden');
+    });
+})
+
+$(function navigateBike() {
+    $(".biking-option").on('click', function(event) {
+        event.preventDefault();
+        $(".main-search-container").removeClass('hidden');
+        $(".results-container").addClass('hidden');
+    });
+})
+
+$(function getResults() {
+    $(".main-search-form").on('submit', function(event) {
+        event.preventDefault();
+        $(".main-search-container").addClass('hidden');
+        $(".results-container").removeClass('hidden');
+    });
+})
+
+$(function getNewResults() {
+    $(".results-search-form").on('submit', function(event) {
+        event.preventDefault();
+        $(".main-search-container").addClass('hidden');
+        $(".results-container").removeClass('hidden');
+    })
+})
+
+$(function showListContent() {
+    $("li").on('click', function(event) {
+        $(this).find(".trail-content").toggleClass('hidden');
+    })
+}) 
