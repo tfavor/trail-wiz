9260d290dd889fb6d4e1f57554cd59f271a44308
Tristan Favor <tristanfavor29@gmail.com>
2019-07-05 13:29:40 -0500

    added functionality to start page

--- script.js
+++ script.js
@@ -1,4 +1,59 @@
+const trailKey = '200507765-d555489db7aa873777c05ace340f460d';
+const appId = 'OnsRRoCMl2ZmKhSIWQ8a';
+const appCode = '96rL5FNqiCEvWqWaoS4QUw';
+let trailsUrl = '';
 
+/* choosing to look for hiking trails at the beginning */
+function chooseHiking() {
+    trailsUrl = 'https://www.hikingproject.com/data/get-trails';
+    event.preventDefault();
+    $("header").addClass('hidden');
+    $(".nav-bar-container").removeClass('hidden');
+    $(".main-search-container").removeClass('hidden');
+    handleSubmit();
+    console.log(trailsUrl);
+}
+/* choosing to look for biking trails at the beginning */
+function chooseBiking() {
+    trailsUrl = 'https://www.mtbproject.com/data/get-trails';
+    event.preventDefault();
+    $("header").addClass('hidden');
+    $(".nav-bar-container").removeClass('hidden');
+    $(".main-search-container").removeClass('hidden');
+    handleSubmit();
+    console.log(trailsUrl);
+}
+
+function getFormValues() {
+    let city = '';
+    let radius = '';
+    event.preventDefault();
+    city = $('.location').val();
+    radius = $('.distance').val();
+    console.log(city);
+    console.log(radius);
+}
+
+$(function choose() {
+    $(".hike, .bike").on('click', function(event) {
+        event.preventDefault();
+        if (this.id == 'hiking') {
+            chooseHiking();
+        } else if (this.id == 'biking') {
+            chooseBiking();
+        }
+    });
+})
+
+function handleSubmit() {
+    $(".search-form").on('submit', function(event) {
+        event.preventDefault();
+        getFormValues()
+    });
+}
+
+
+/*
 $(function changeHike() {
     $(".hike").on('click', function(event) {
         event.preventDefault();
@@ -54,3 +109,5 @@ $(function showListContent() {
         $(this).find(".trail-content").toggleClass('hidden');
     })
 }) 
+*/
+
